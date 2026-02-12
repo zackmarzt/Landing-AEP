dda01536d91c3ae8900302c7c47e4ee78a85b53a
zackmarzt <marzinkowskizack@gmail.com>
2026-02-12 11:24:21 -0300

    Fix URL images

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bosquemananciais.org.br',
         port: '',
         pathname: '/**',
       },
